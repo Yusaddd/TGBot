625d83422248495d3ca44db458cc310c360a54e0
Yuriy Porokhin <porohinurij@gmail.com>
2025-06-19 15:07:57 +0500

    разделил на 2 класса

---
+++ Interaction.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using MySqlConnector;
+using static ConsoleApp25.Buttons;
+
+namespace ConsoleApp25
+{
+    internal class Interaction
+    {
+        #region текстовые константы
+        // конст для регистрации
+        private const string reg_text1 = "Есть аккаунт"; private const string reg_text2 = "Нет аккаунта";
+
+        // конст для вводного сообщения
+        private const string text1 = "Выбрать клинику"; private const string text2 = "Информация о клинике";
+        private const string text3 = "Записаться на приём"; private const string text4 = "Отменить приём";
+        private const string text5 = "Просмотр докторов клиники"; private const string text6 = "Просмотр новостей клиники";
+        private const string text7 = "Справочная служба"; private const string text8 = "FAQ";
+        private const string text9 = "Информация о пользователе";
+        #endregion
+
+        private static readonly Dictionary<long, bool> _sentWelcomeMessages = new Dictionary<long, bool>();
+        internal static async Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            //var button = update.CallbackQuery;
+
+            // Проверяем, отправил ли бот приветствие этому пользователю ранее
+            if (!_sentWelcomeMessages.ContainsKey(message?.Chat?.Id ?? 0)) // Если нет, то...
+            {
+                await SendWelcomeMessage(client, message); // Отправляем приветствие
+                _sentWelcomeMessages.Add(message?.Chat?.Id ?? 0, true); // Отмечаем, что приветствие отправлено
+            }
+
+            // Обработка сообщений и команд
+            if (message?.Text != null && !string.IsNullOrWhiteSpace(message.Text))
+            {
+                // Если пользователь вводит команду
+                if (message.Text.ToLower().Contains("/"))
+                {
+                    switch (message.Text.ToLower())
+                    {
+                        case "/appointment":
+                            await AppointmentCommand(client, message);
+                            break;
+                        case "/cancel":
+                            await CancelCommand(client, message);
+                            break;
+                        case "/clinics":
+                            await ClinicsCommand(client, message);
+                            break;
+                        case "/info":
+                            await InfoCommand(client, message);
+                            break;
+                        case "/doctors":
+                            await DoctorsCommand(client, message);
+                            break;
+                        case "/faq":
+                            await FaqCommand(client, message);
+                            break;
+                        case "/help":
+                            await HelpCommand(client, message);
+                            break;
+                        case "/news":
+                            await NewsCommand(client, message);
+                            break;
+                        case "/user":
+                            await UserCommand(client, message);
+                            break;
+
+                        default:
+                            if (!_sentWelcomeMessages.ContainsKey(message.Chat.Id))
+                                await client.SendTextMessageAsync(message.Chat.Id, "Мы получили ваше сообщение, отправьте ещё!");
+                            break;
+                    }
+                }
+
+                else if (message.Text.ToLower().Contains("выбрать клинику"))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                    ClinicsCommand(client, message);
+                }
+                else if (message.Text.ToLower().Contains("информация") && message.Text.ToLower().Contains("клинике"))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                    InfoCommand(client, message);
+                }
+                else if (message.Text.ToLower().Contains("записаться на прием") || message.Text.ToLower().Contains("записаться на приём"))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                    AppointmentCommand(client, message);
+                }
+                else if ((message.Text.ToLower().Contains("отменить") && message.Text.ToLower().Contains("прием")) || (message.Text.ToLower().Contains("отменить") && message.Text.ToLower().Contains("приём")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                    await client.SendTextMessageAsync(message.Chat.Id, "выберите запись, которую хотите отменить:");
+                }
+                else if ((message.Text.ToLower().Contains("докторы")) || (message.Text.ToLower().Contains("докторов")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+
+                }
+                else if ((message.Text.ToLower().Contains("записаться на прием")) || (message.Text.ToLower().Contains("записаться на приём")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                }
+                else if ((message.Text.ToLower().Contains("записаться на прием")) || (message.Text.ToLower().Contains("записаться на приём")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                }
+                else if ((message.Text.ToLower().Contains("записаться на прием")) || (message.Text.ToLower().Contains("записаться на приём")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                }
+                else if ((message.Text.ToLower().Contains("записаться на прием")) || (message.Text.ToLower().Contains("записаться на приём")))
+                {
+                    await client.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                }
+
+                // Если пользователь вводит приветствие
+                else if (message.Text.ToLower().Contains("здравствуйте") || message.Text.ToLower().Contains("приветствую") || message.Text.ToLower().Contains("салам алейкум")
+                     || message.Text.ToLower().Contains("доброе утро") || message.Text.ToLower().Contains("добрый день") || message.Text.ToLower().Contains("добрый вечер")
+                     || message.Text.ToLower().Contains("у меня есть вопрос"))
+                {
+                    // Функция отправки фотографии пользователю
+                    string imagePath = Path.Combine(Environment.CurrentDirectory, "котик.jpg");
+                    using (var stream = System.IO.File.OpenRead(imagePath))
+                    {
+                        var result = client.SendPhotoAsync(message.Chat.Id, new InputOnlineFile(stream), caption: "Держите котика для хорошего настроения <3").Result;
+                    }
+
+                    await client.SendTextMessageAsync(message.Chat.Id, "Здравствуйте! Могу помочь вам в следующих услугах:",
+                        replyMarkup: GetNineButtons(text1, text2, text3, text4, text5, text6, text7, text8, text9));
+                }
+
+                // Если пользователь вводит что-то иное
+                else await client.SendTextMessageAsync(message.Chat.Id, "Простите, я не знаю, что вам на это ответить, я умею общаться только на темы, " +
+                    "связанные с клиниками, докторами и прочим.\nПожалуйста, выберите, что вас интересует:",
+                    replyMarkup: GetNineButtons(text1, text2, text3, text4, text5, text6, text7, text8, text9));
+            }
+            else
+            {
+                await client.SendTextMessageAsync(message?.Chat?.Id ?? 0, "Пожалуйста, введите не пустое сообщение!");
+            }
+
+            //// Обработка inline кнопок
+            //if (button != null)
+            //{
+            //    if (update.CallbackQuery.Data == reg_text1)
+            //    {
+            //        var result = client.SendTextMessageAsync(button.Message.Chat.Id, "Введите ваш логин и пароль через запятую").Result;
+            //    }
+            //    if (update.CallbackQuery.Data == reg_text2)
+            //    {
+            //        var result = client.SendTextMessageAsync(button.Message.Chat.Id, "Сейчас мы отправим вам форму для регистрации").Result;
+            //    }
+            //    if (update.CallbackQuery.Data == text1)
+            //    {
+            //        var result = client.SendTextMessageAsync(button.Message.Chat.Id, "Ща запишем вас епьа").Result;
+            //    }
+            //}
+        }
+        #region команды клавиатуры
+        private static async Task AppointmentCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Можно записаться на приём в следующие даты:");
+            // Добавьте здесь логику для обработки команды /appointment
+        }
+        private static async Task CancelCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Выберите запланированный приём на отмену записи:");
+            // Добавьте здесь логику для обработки команды /cancel
+        }
+        private static async Task ClinicsCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Вот список доступных клиник:");
+            // Добавьте здесь логику для обработки команды /clinics
+        }
+        private static async Task InfoCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Информация о клинике:");
+            // Добавьте здесь логику для обработки команды /info
+        }
+        private static async Task DoctorsCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Информация о докторах в данной клинике:");
+            // Добавьте здесь логику для обработки команды /doctors
+        }
+        private static async Task FaqCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Вот ответы на часто задаваемые вопросы:");
+            // Добавьте здесь логику для обработки команды /faq
+        }
+        private static async Task HelpCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Справочная служба:");
+            // Добавьте здесь логику для обработки команды /help
+        }
+        private static async Task NewsCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Новости клиники:");
+            // Добавьте здесь логику для обработки команды /news
+        }
+        private static async Task UserCommand(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Ваша информация:");
+            // Добавьте здесь логику для обработки команды /user
+        }
+        #endregion
+        private static async Task SendWelcomeMessage(ITelegramBotClient client, Message message)
+        {
+            await client.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: $"Здравствуйте! Я — Бот, который может:\n\n" +
+                      "--Помочь с выбором клиники\n" +
+                      "--Записать на прием к врачу\n" +
+                      "--Задать вопрос напрямую врачу и получить от него ответ\n" +
+                      "--Делиться свежими новостями из мира медицины\n\n" +
+                      "Вы уже зарегистрированы?",
+                replyMarkup: GetTwoButtons(reg_text1, reg_text2));
+        }
+        internal static Task Error(ITelegramBotClient client, Exception err, CancellationToken ctoken)
+        {
+            WriteLine($"Ошибка: {err.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
